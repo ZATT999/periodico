f98c766ab5084747788c597076758f1804821d7d
zatt999 <zatt999mc@gmail.com>
2025-04-08 20:20:48 -0500

    hover for new notice

--- src/pages/noticias.jsx
+++ src/pages/noticias.jsx
@@ -25,18 +25,20 @@ export default function NoticiasPage() {
     <>
       <h1 className="titlePage">CTE: visiones empresariales</h1>
       <main>
-        <section className="ultimaNoticia">
-          <img
-            src={noticiaActual.images[0].src}
-            alt={noticiaActual.images[0].alt}
-          />
+        <Link to={`noticia/${noticiaActual.slug}`}>
+          <section className="ultimaNoticia">
+            <img
+              src={noticiaActual.images[0].src}
+              alt={noticiaActual.images[0].alt}
+            />
 
-          <div className="carouselContent">
-            <h2>{noticiaActual.title}</h2>
-            <h3>{noticiaActual.date}</h3>
-            <p>{noticiaActual.description}</p>
-          </div>
-        </section>
+            <div className="carouselContent">
+              <h2>{noticiaActual.title}</h2>
+              <h3>{noticiaActual.date}</h3>
+              <p>{noticiaActual.description}</p>
+            </div>
+          </section>
+        </Link>
 
         <h1 className="title">Noticias</h1>
         <MapNoticias />
