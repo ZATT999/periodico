f1a2939d590f90b59ad13eff7a64deff9f546141
zatt999 <zatt999mc@gmail.com>
2025-04-20 15:56:05 -0500

    change style for mansonry

--- src/components/mapNews.jsx
+++ src/components/mapNews.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router"
-import Masonry from "react-masonry-css"
 import { useContext } from "react"
 import { NewsContext } from "../context/context"
+import { DateIcon } from "./icons"
 
 export default function MapNews() {
   const { news } = useContext(NewsContext)
@@ -12,19 +12,31 @@ export default function MapNews() {
     <div className="bg-white min-h-screen px-6 py-10">
       <h1 className="text-2xl font-bold text-black mb-6">Últimas noticias</h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="mansory mx-auto">
         {getNewsVisible.map((news) => (
           <Link to={`/noticia/${news._id}`} key={news._id}>
-            <div className="relative w-full h-60 rounded-2xl overflow-hidden shadow-md border border-gray-200 transition-transform hover:scale-105 duration-200">
+            <div className="w-[250px] h-auto  mb-[10px]  rounded-2xl overflow-hidden shadow-md border-[2px] border-blue-200 transition-transform hover:scale-101 duration-200 ">
               <img
                 src={news.image}
                 alt="Evento"
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover "
               />
-              <div className="absolute bottom-0 w-full bg-gradient-to-t from-white/90 to-transparent px-4 py-3">
-                <h3 className="text-sm font-semibold text-black">
+              <div className="w-full flex flex-col gap-4 px-4 py-5 bg-blue-50 ">
+                <h3 className="text-[15px] font-semibold text-black">
                   {news.title}
                 </h3>
+
+                <div className="flex justify-between items-center  mt-3">
+                  <img
+                    src={news.author.avatar}
+                    alt={`${news.author.name}`}
+                    className="w-7 h-7 rounded-full border-1 border-blue-500"
+                  />
+
+                  <span className="text-[17px] flex gap-1 items-center">
+                    <DateIcon size={17} /> {news.date}
+                  </span>
+                </div>
               </div>
             </div>
           </Link>
