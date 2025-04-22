37064c288ad9bd3ba9266b388982393d274425ae
zatt999 <zatt999mc@gmail.com>
2025-04-21 20:57:37 -0500

    fix import

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,6 @@
 import { useContext, useState } from "react"
 import { useNavigate } from "react-router"
 import { UserContext } from "../context/context"
-import User from "../../../backEnd/models/user"
 
 export default function Login() {
   const [error, setError] = useState("")
