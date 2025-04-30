61629ba059e49ec08220b4cf7f1c47fe8c705efe
zatt999 <zatt999mc@gmail.com>
2025-04-30 17:21:11 -0500

    reset scroll in new page

--- src/pages/id.jsx
+++ src/pages/id.jsx
@@ -6,6 +6,7 @@ import { useContext } from "react"
 export default function NewsId() {
   const params = useParams()
   const { news } = useContext(NewsContext)
+  window.scrollTo(0, 0)
 
   const newsFilter = news.find((newsItem) => newsItem.id === params.slug)
 
