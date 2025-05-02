f5a65b4d212635edbe45a4780b6855e73341d36d
zatt999 <zatt999mc@gmail.com>
2025-05-02 16:51:03 -0500

    change url for fetchs

--- src/utils/urlForFetchs.js
+++ src/utils/urlForFetchs.js
@@ -2,5 +2,5 @@ export const urlForFetchs = () => {
   if (process.env.NODE_ENV === "development") {
     return `http://localhost:3000`
   }
-  return `https://periodico-backend.onrender.com`
+  return `https://cuddly-bunny-zatt999-cb2b7e0f.koyeb.app`
 }
