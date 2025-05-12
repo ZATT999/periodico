9051a8b3f700ec4498523762b348085a3d1a05ee
zatt999 <zatt999mc@gmail.com>
2025-05-12 18:31:40 -0500

    new const

--- src/const/urlfetch.js
+++ src/const/urlfetch.js
@@ -1,1 +1,4 @@
-export const urlFetchs = `https://numerous-aundrea-zatt999-e35d9329.koyeb.app`
+export const urlFetchs =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://numerous-aundrea-zatt999-e35d9329.koyeb.app"
